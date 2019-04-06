47a1dcdae71913969ced31334bcad4987cb9776b
frankburleypcc <frank.burley@pcc.edu>
2019-04-06 11:44:06 -0700

    set a ternary operator for play pause in a toggle function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,13 +1,16 @@
 // get Elements
 const player = document.querySelector('.player');
-const video = player.querySelector('viewer');
-const progress = player.querySelector('progress');
-const progressBar = player.querySelector('progress_filled');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('player_slider');
+const ranges = player.querySelectorAll('player__slider');
 
 // Build functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+};
 
 //hook up the event listeners
